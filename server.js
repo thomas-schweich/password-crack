bcf46ac31168f009ca3277f14fb744de43349d9e
Thomas Schweich <thomas.schweich@gmail.com>
2018-09-06 19:09:56 -0400

    Initial hacker.js implementation with sample server

---
+++ server.js
@@ -0,0 +1,45 @@
+var http = require('http')
+  , fs   = require('fs')
+  , url  = require('url')
+  , port = 8080;
+
+var server = http.createServer (function (req, res) {
+  var uri = url.parse(req.url)
+
+  switch( uri.pathname ) {
+    case '/':
+      sendFile(res, 'index.html')
+      break
+    case '/index.html':
+      sendFile(res, 'index.html')
+      break
+    case '/mydata.json':
+      sendJson(res, './mydata.json')
+      break
+    default:  
+      res.end('404 not found')
+  }
+})
+
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
+
+// subroutines
+
+function sendFile(res, filename) {
+
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': 'text/html'})
+    res.end(content, 'utf-8')
+  })
+
+}
+
+function sendJson(res, filename) {
+
+    fs.readFile(filename, function(error, content) {
+      res.writeHead(200, {'Content-type': 'application/json'})
+      res.end(content, 'utf-8')
+    })
+  
+  }
