5d5eefe071edeee5584ada421070a02ed09e2309
Alex <adhard@wpi.edu>
2018-09-09 22:40:56 -0400

    Create new client

--- server.js
+++ server.js
@@ -21,12 +21,20 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
 });
 
 function addSequences(seqobj) {
-    client.connect()
-    client.query('SELECT pat FROM patterns ORDER BY frq DESC;', (err,res) => {
+    const client2 = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+    console.log("seq 1")
+    client2.connect()
+    console.log("seq 2")
+    client2.query('SELECT pat FROM patterns ORDER BY frq DESC;', (err,res) => {
         // Whats up with this
-        
+        console.log("seq 3")
         if (err) throw err;
+        console.log("seq 4")
         seqobj.SEQUENCES = res.rows
+        console.log("seq 5")
         client.end();
     })
 }
@@ -48,10 +56,15 @@ var server = http.createServer(function (req, res) {
             sendFile(res, 'hacker.js')
             break
         case '/passwords.json':
+            console.log("err 1")
             var response;
+            console.log("err 2")
             addSequences(response)
+            console.log("err 3")
             res.writeHead(200, { 'Content-type': 'application/json' })
+            console.log("err 4")
             res.end(JSON.stringify(response), 'utf-8')
+            console.log("err 5")
             break
         default:
             res.end('404 not found')
